376533d93d176ef8dd5ea526a4aacd758a379124
Пілецький Володимир <piletskiy10@gmail.com>
2022-03-18 21:37:37 +0200

    ship movement added

--- alien-invasion.py
+++ alien-invasion.py
@@ -17,16 +17,38 @@ class AlienInvasion:
 
         self.ship = Ship(self)
 
+    def _check_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
+                sys.exit()
+            if event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+            if event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
+
+    def _check_keydown_events(self, event):
+        if event.key == pygame.K_d:
+            self.ship.moving_right = True
+        if event.key == pygame.K_a:
+            self.ship.moving_left = True
+
+    def _check_keyup_events(self, event):
+        if event.key == pygame.K_d:
+            self.ship.moving_right = False
+        if event.key == pygame.K_a:
+            self.ship.moving_left = False
+
+    def _update_screen(self):
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+        pygame.display.flip()
+
     def run_game(self):
         while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
-
-            pygame.display.flip()
+            self._check_events()
+            self.ship.update()
+            self._update_screen()
 
 
 if __name__ == '__main__':
--- ship.py
+++ ship.py
@@ -16,5 +16,20 @@ class Ship:
         """Разместить нижний центр корабля в точке нижнего центра экрана игры"""
         self.rect.midbottom = self.screen_rect.midbottom
 
+        self.moving_right = False
+        self.moving_left = False
+
+        self.settings = ai_game.settings
+
+        self.x = float(self.rect.x)
+
+    def update(self):
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+
+        self.rect.x = self.x
+
     def blitme(self):
         self.screen.blit(self.image, self.rect)
